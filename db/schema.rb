9ba7bcf762178ed5253e3af367c7dc7c5ffcdf99
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-06-05 12:55:21 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_012422) do
+ActiveRecord::Schema.define(version: 2018_06_05_033010) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
